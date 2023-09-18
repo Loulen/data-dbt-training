634e6fde0a9c1d4a2e349772117a70533570311b
jnadal <j.nadal@betclicgroup.com>
2023-09-18 16:45:29 +0200

    feature: jinja

--- models/staging/base/base_fuzzy_orders.sql
+++
@@ -1,8 +0,0 @@
-select distinct
-    parse_json(dishes_names)::array as dishes_names
-    {# , dishes_ids #}
-    , payment_method
-    , amount
-    , created_at
-from 
-    {{ source('sources', 'fuzzy_orders') }}
--- models/staging/stg_orders__dishes_flattened.sql
+++
@@ -1,5 +0,0 @@
-select
-    o.*
-    , d.value as dishes_id
-from {{ref('base_orders')}} as o
-    ,table(flatten(input => o.dishes_ids, outer => true)) as d
